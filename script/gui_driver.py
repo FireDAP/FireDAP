41f6295189855a7ad07bc5d52f17ae7aac775c19
pallasmanul <pallasmanul@outlook.com>
2023-07-27 18:20:56 +0800

    change  project

---
+++ script/gui_driver.py
@@ -0,0 +1,90 @@
+import lvgl as lv
+
+class Display_Driver:
+    def __init__( self, width, height, lcd, tsc, fb_rows=64 ):
+        self.width = width
+        self.height = height
+        self.lcd = lcd
+        self.tsc = tsc
+        self.fb_rows = fb_rows
+        
+        self.is_fb1 = True
+        self.dma_running = False
+        
+        self.x = 0
+        self.y = 0
+        self.s = 0
+        
+        self.init_gui()
+    
+    def init_gui( self ):
+        lv.init()
+        
+        self.disp_drv = lv.disp_create(self.width, self.height)
+        
+        self.fb1 = bytearray( self.width*self.fb_rows )
+        self.fb2 = bytearray( self.width*self.fb_rows )
+        print( "len( fb1 )", len( self.fb1 ) )
+        print( "len( fb2 )", len( self.fb2 ) )
+        
+        print( "disp set draw buffers" )
+        self.disp_drv.set_draw_buffers(self.fb1, self.fb2, len(self.fb1),lv.DISP_RENDER_MODE.PARTIAL)
+
+        print( "disp set flash cb")
+        self.disp_drv.set_flush_cb(self.disp_drv_flush_cb)
+
+#         print( "disp set indev" )
+#         self.indev_drv = lv.indev_t()
+#         self.indev_drv.set_disp(self.disp_drv)
+#         self.indev_drv.set_type(lv.INDEV_TYPE.POINTER)
+#         self.indev_drv.set_read_cb(self.indev_drv_read_cb)
+
+
+    def disp_drv_flush_cb( self, disp_drv, area, color ):
+        print( "disp_drv_flush_cb", area.x1, area.x2, area.y1, area.y2 )
+        
+        if( self.dma_running == True ):
+            self.lcd.wait_dma()
+            self.dma_running = False
+        
+        if( self.is_fb1 ):
+            fb = memoryview( self.fb1 )
+        else:
+            fb = memoryview( self.fb2 )
+        self.is_fb1 = not self.is_fb1
+        
+        x = area.x1
+        y = area.y1
+        w = area.x2 - area.x1 + 1
+        h = area.y2 - area.y1 + 1
+        self.lcd.draw_bitmap_dma( x, y, w, h, fb[0:w*h*lv.color_t.__SIZE__], is_blocking=False )
+        self.dma_running = True
+
+        disp_drv.flush_ready()
+        
+    def indev_drv_read_cb( self, indev_drv, data ):
+        #print( "indev_drv_read_cb", self.x, self.y, self.s )
+        
+        if( self.dma_running == True ):
+            self.lcd.wait_dma()
+            self.dma_running = False     
+        
+        self.tsc.spi_init()
+        x, y = self.tsc.read()
+        self.lcd.spi_init() # Reinit SPI with LCD settings
+        
+        if( x or y ):
+            self.x = x
+            self.y = y
+            self.s = 1
+            #print( "indev_drv_read_cb", self.x, self.y, self.s )
+        else:
+            self.s = 0
+        
+        data.point.x = self.x
+        data.point.y = self.y
+        data.state = self.s
+        
+        return False
+
+print("done")
---
+++ script/main.py
@@ -0,0 +1,83 @@
+import time
+time.sleep(2)
+import machine
+import uasyncio as asyncio
+import lvgl as lv
+from hal.st7789 import *
+from gui.async_utils import Lv_Async
+from gui_driver import Display_Driver
+from kiwidap_ui import KiwiDAP_UI
+from ili9xxx import *
+from st77xx import St7735,ST77XX_PORTRAIT
+
+import gc
+gc.collect()
+gc.threshold( gc.mem_free() // 4 + gc.mem_alloc() )
+
+
+lcd_baudrate = 24_000_000
+tsc_baudrate = 1_000_000
+adc_frequency = 10_000_000
+pwm_frequency = 1_000_000
+
+spi_sck = machine.Pin( 10, machine.Pin.OUT )
+spi_mosi= machine.Pin( 11, machine.Pin.OUT )
+spi_miso= machine.Pin( 12, machine.Pin.IN   )
+spi_dc  = machine.Pin( 8, machine.Pin.OUT )
+
+lcd_rst = machine.Pin( 15, machine.Pin.OUT )
+lcd_bl  = machine.Pin( 13, machine.Pin.OUT )
+lcd_cs  = machine.Pin( 9, machine.Pin.OUT )
+
+
+
+# Calibration values
+print( "tsc()" )
+tsc = None
+
+
+# Init lcd last one to start lvgl with SPI LCD baudrate
+print( "lcd()" )
+# lcd = St7735(
+#     mhz=3, mosi=11, clk=10, cs=9, dc=8, rst=15, power=-15, backlight=13, backlight_on=13)
+
+lcd = St7789( lcd_baudrate, lcd_cs, spi_sck, spi_mosi, spi_miso, spi_dc, lcd_rst, lcd_bl )
+#  w, h = 160//4, 80//4
+#  bmp = build_square_buf( w, h )
+#  
+#  t0 = time.ticks_us()
+#  lcd.draw_bitmap_dma( 30, 60, w, h, bmp )
+#  t1 = time.ticks_us()
+#  
+#  print( "Maximum FPS @24MHz:", 24e6/( 160*80*16 ) ) # FPS = F/(W*H*BPP)
+#  print( "Achieved FPS:", 1/(16*(t1-t0)*1e-6) )       # Note: Test only draws 1/16 of the sreen area
+#  
+#  print( "Draw TSC calibration pattern")
+#  w, h = 100, 100
+#  bmp = build_square_buf( w, h )
+lv.deinit()
+display_driver = Display_Driver( 160, 80, lcd, tsc )
+
+    
+try:
+    scr = lv.scr_act()
+    scr_style = lv.style_t()
+    scr_style.set_bg_color(lv.color_hex(0x003a00))
+    scr.add_style(scr_style,0)
+              
+    label = lv.label(lv.scr_act())
+    label_style = lv.style_t()
+    label_style.set_text_color(lv.color_hex(0xffffff))
+    label.set_text("Hello world")
+    label.add_style(label_style,0)
+    label.align(lv.ALIGN.CENTER, 0, 0)
+    
+    lv.scr_load(scr)
+        
+    print( "run" )
+    lva = Lv_Async(refresh_rate=20 )
+    asyncio.Loop.run_forever()
+    
+except Exception as error:
+    print(error)
+    lv.deinit()
